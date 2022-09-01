3b28f142e9cd7052b23712bab8056b50e542ad35
Konrad <konrad.michal.dumin@gmail.com>
2022-09-01 14:35:01 +0200

    New constructor for ChessSquare

--- Model/ChessSquare.cs
+++ Model/ChessSquare.cs
@@ -17,22 +17,38 @@ namespace QuickChess.Model
             Column = column;
             IsOccupied = false;
             IsLegalNextMove = false;
+            CurrPiece = null;
         }
 
-        public ChessSquare (uint row, uint column, bool isOccupied)
+        public ChessSquare (uint row, uint column, bool isOccupied, BoardPiece currPiece)
         {
+            if (isOccupied && currPiece == null)
+                throw new ArgumentException("The square is occuppied but the currPiece value is null.");
             Row = row;
             Column = column;
             IsOccupied = isOccupied;
             IsLegalNextMove = false;
+            CurrPiece = currPiece;
         }
 
-        public ChessSquare (uint row, uint column, bool isOccupied, bool isLegalNextMove)
+        public ChessSquare (uint row, uint column, bool isLegalNextMove)
         {
+            Row = row;
+            Column = column;
+            IsOccupied = false;
+            IsLegalNextMove = isLegalNextMove;
+            CurrPiece = null;
+        }
+
+        public ChessSquare (uint row, uint column, bool isOccupied, bool isLegalNextMove, BoardPiece currPiece)
+        {
+            if (isOccupied && currPiece == null)
+                throw new ArgumentException("The square is occuppied but the currPiece value is null.");
             Row = row;
             Column = column;
             IsOccupied = isOccupied;
             IsLegalNextMove = isLegalNextMove;
+            CurrPiece = currPiece;
         }
     }
 }
