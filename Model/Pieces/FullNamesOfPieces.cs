f772cda8b51ed7c5b6d92fa1463bc91ac5c936a4
Konrad <konrad.michal.dumin@gmail.com>
2022-09-04 16:16:18 +0200

    Complete Piece enums

--- Model/Pieces/FullNamesOfPieces.cs
+++ Model/Pieces/FullNamesOfPieces.cs
@@ -3,6 +3,11 @@ namespace QuickChess.Model.Pieces
     public static class FullNamesOfPieces
     {
         public const string None = "None";
+        public const string Pawn = "Pawn";
         public const string Knight = "Knight";
+        public const string Bishop = "Bishop";
+        public const string Rook = "Rook";
+        public const string Queen = "Queen";
+        public const string King = "King";
     }
 }
--- Model/Pieces/ShortNamesOfPieces.cs
+++ Model/Pieces/ShortNamesOfPieces.cs
@@ -4,6 +4,11 @@ namespace QuickChess.Model.Pieces
     {
         // Square
         public const char None = '\u25A1';
+        public const char Pawn = 'P';
         public const char Knight = 'N';
+        public const char Bishop = 'B';
+        public const char Rook = 'R';
+        public const char Queen = 'Q';
+        public const char King = 'K';
     }
 }
