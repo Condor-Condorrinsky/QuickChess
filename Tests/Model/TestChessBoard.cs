bc209789bc1685f0dffa4817ed9ce3692ee44a9f
Konrad <konrad.michal.dumin@gmail.com>
2022-09-06 14:11:47 +0200

    First unit tests of PutPiece()

--- Tests/Model/TestChessBoard.cs
+++ Tests/Model/TestChessBoard.cs
@@ -10,17 +10,21 @@ namespace QuickChess.Tests.Model
         const int CLASSIC_CHESSBOARD_SIZE = 8;
 
         [Test]
-        public void TestPrintBoard ()
+        public void TestPutPiece ()
         {
             ChessBoard chessBoard = new ChessBoard(CLASSIC_CHESSBOARD_SIZE);
 
-            chessBoard.PutPiece(2, 7, new BoardPiece(FullNamesOfPieces.Bishop, ShortNamesOfPieces.Bishop));
-            chessBoard.PutPiece(0, 5, new BoardPiece(FullNamesOfPieces.Knight, ShortNamesOfPieces.Knight));
-            chessBoard.PutPiece(3, 4, new BoardPiece(FullNamesOfPieces.Queen, ShortNamesOfPieces.Queen));
-            chessBoard.PutPiece(7, 0, new BoardPiece(FullNamesOfPieces.King, ShortNamesOfPieces.King));
-            chessBoard.PutPiece(4, 4, new BoardPiece(FullNamesOfPieces.Pawn, ShortNamesOfPieces.Pawn));
+            BoardPiece bishop = new BoardPiece(FullNamesOfPieces.Bishop, ShortNamesOfPieces.Bishop);
+            BoardPiece king = new BoardPiece(FullNamesOfPieces.King, ShortNamesOfPieces.King);
+            BoardPiece queen = new BoardPiece(FullNamesOfPieces.Queen, ShortNamesOfPieces.Queen);
 
-            Assert.True(chessBoard.Grid[2,7].Piece.Equals(new BoardPiece(FullNamesOfPieces.Bishop, ShortNamesOfPieces.Bishop)));
+            chessBoard.PutPiece(0, 1, bishop);
+            chessBoard.PutPiece(5, 7, king);
+            chessBoard.PutPiece(5, 3, queen);
+
+            Assert.AreEqual(chessBoard.Grid[0,1].Piece.FullName, bishop.FullName);
+            Assert.AreEqual(chessBoard.Grid[5,7].Piece.FullName, king.FullName);
+            Assert.AreEqual(chessBoard.Grid[5,3].Piece.FullName, queen.FullName);
         }
     }
 }
