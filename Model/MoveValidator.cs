26314f12cee63f352d5ce03e14384382c11c5959
Konrad <konrad.michal.dumin@gmail.com>
2022-09-12 12:38:43 +0200

    ValidateRookMove

--- Model/MoveValidator.cs
+++ Model/MoveValidator.cs
@@ -1,3 +1,5 @@
+using QuickChess.Model.Pieces;
+
 namespace QuickChess.Model
 {
     public class MoveValidator
@@ -6,6 +8,19 @@ namespace QuickChess.Model
 
         // TODO: finish the body
         public bool IsMoveValid()
+        {
+            return false;
+        }
+
+        public bool ValidateRookMove(Coordinates from, Coordinates to)
+        {
+            if ((from.Row == to.Row) && (from.Column != to.Column)) return true;
+            if ((from.Row != to.Row) && (from.Column == to.Column)) return true;
+
+            return false;
+        }
+
+        public bool ValidateBishopMove(Coordinates from, Coordinates to)
         {
             return false;
         }
