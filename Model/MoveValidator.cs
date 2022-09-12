ab8d4592ce50d1561bc2d83468bbb7041ed71b96
Konrad <konrad.michal.dumin@gmail.com>
2022-09-12 13:46:22 +0200

    additional comments

--- Model/MoveValidator.cs
+++ Model/MoveValidator.cs
@@ -12,6 +12,22 @@ namespace QuickChess.Model
             return false;
         }
 
+        public bool ValidateKingMove(Coordinates from, Coordinates to)
+        {
+            const int KINGS_RANGE = 2;
+
+            if (Math.Abs(to.Row - from.Row) < KINGS_RANGE && (Math.Abs(to.Column - from.Column)) < KINGS_RANGE) return true;
+
+            return false;
+        }
+
+        public bool ValidateQueenMove(Coordinates from, Coordinates to)
+        {
+            if (ValidateBishopMove(from, to) || ValidateRookMove(from, to)) return true;
+
+            return false;
+        }
+
         public bool ValidateRookMove(Coordinates from, Coordinates to)
         {
             if ((from.Row == to.Row) && (from.Column != to.Column)) return true;
@@ -21,6 +37,23 @@ namespace QuickChess.Model
         }
 
         public bool ValidateBishopMove(Coordinates from, Coordinates to)
+        {
+            if ((from.Row == to.Row) && (from.Column == to.Column)) return false;
+            if (Math.Abs(to.Row - from.Row) == (Math.Abs(to.Column - from.Column))) return true;
+
+            return false;
+        }
+
+        public bool ValidateKnightMove(Coordinates from, Coordinates to)
+        {
+            if ((Math.Abs(to.Row - from.Row) == 1) && (Math.Abs(to.Column - from.Column) == 2)) return true;
+            if ((Math.Abs(to.Row - from.Row) == 2) && (Math.Abs(to.Column - from.Column) == 1)) return true;
+
+            return false;
+        }
+
+        // TODO: finish implementation
+        public bool ValidatePawnMove(Coordinates from, Coordinates to, Colour cl)
         {
             return false;
         }
