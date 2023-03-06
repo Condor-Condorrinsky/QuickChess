2d168fa6ef73647ea3c6bb94e29d9f4f10111dda
Konrad <konrad.michal.dumin@gmail.com>
2023-03-06 17:16:51 +0100

    GameController and KingSquaresValidator stubs

---
+++ Controller/GameController.cs
@@ -0,0 +1,11 @@
+using QuickChess.Model;
+using QuickChess.Model.Pieces;
+
+namespace QuickChess.Controller
+{
+    // The main class of the Controller namespace
+    public class GameController
+    {
+        
+    }
+}
