5f8003f5d5604af19c9af50c407479f9bce8e4fc
Konrad <konrad.michal.dumin@gmail.com>
2023-03-06 17:28:48 +0100

    equals and hashcode for coordinates

--- Model/Pieces/Coordinates.cs
+++ Model/Pieces/Coordinates.cs
@@ -12,5 +12,38 @@ namespace QuickChess.Model.Pieces
             this.Row = row;
             this.Column = column;
         }
+
+        public override bool Equals(object? obj)
+        {
+            Coordinates coords;
+
+            if (obj == null) return false;
+
+            if (!(obj is Coordinates)) return false;
+            else coords = (Coordinates) obj;
+
+            if (Row != coords.Row) return false;
+            if (Column != coords.Column) return false;
+
+            return true;
+        }
+
+        public override int GetHashCode()
+        {
+            //https://stackoverflow.com/questions/263400/what-is-the-best-algorithm-for-overriding-gethashcode
+            const int STACK_OVERFLOW_MAGIC_NUMBER1 = 17;
+            const int STACK_OVERFLOW_MAGIC_NUMBER2 = 23;
+
+            unchecked
+            {
+                int hash = STACK_OVERFLOW_MAGIC_NUMBER1;
+
+                hash = hash * STACK_OVERFLOW_MAGIC_NUMBER2 + Row.GetHashCode();
+                hash = hash * STACK_OVERFLOW_MAGIC_NUMBER2 + Column.GetHashCode();
+
+                return hash;
+            }
+        }
+
     }
 }
