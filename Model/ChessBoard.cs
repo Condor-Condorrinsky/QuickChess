2c2006b61d35dcf2917949846017c977dc863e8f
Konrad <konrad.michal.dumin@gmail.com>
2023-03-07 13:55:03 +0100

    RemovePiece, PiecesInGame Dict, edge cases checking

--- Model/ChessBoard.cs
+++ Model/ChessBoard.cs
@@ -4,57 +4,78 @@ namespace QuickChess.Model
 {
     public class ChessBoard
     {
+        const int MAX_PIECES_IN_CHESS_GAME = 32;
+
         public int Size { get; set; }
         public ChessSquare[,] Grid { get; set; }
+        public Dictionary<Coordinates, BoardPiece> PiecesInGame { get; }
         private PieceFactory Factory { get; }
 
         public ChessBoard (int size)
         {
             this.Size = size;
             this.Grid = new ChessSquare[size, size];
+            this.PiecesInGame = new Dictionary<Coordinates, BoardPiece>(MAX_PIECES_IN_CHESS_GAME);
             this.Factory = new PieceFactory();
 
             for (int i = 0; i < size; i++)
             {
                 for (int j = 0; j < size; j++)
                 {
-                    // PutPiece(i, j, Factory.GetDefaultPiece());
-                    Grid[i, j] = new ChessSquare(Factory.GetDefaultPiece());
+                    // PutPiece(i, j, Factory.GetEmptyPiece());
+                    Grid[i, j] = new ChessSquare(Factory.GetEmptyPiece());
                 }
             }
         }
 
         public BoardPiece GetPiece(int row, int column)
         {
-            if (row < 0 || column < 0 || row > (Size-1) || column > (Size-1)) throw new IndexOutOfRangeException();
+            if (row < 0 || column < 0 || row > (Size-1) || column > (Size-1))
+                throw new IndexOutOfRangeException("Specified square does not exist!");
 
             return this.Grid[row, column].Piece;
         }
 
         public BoardPiece GetPiece(Coordinates coords)
         {
-            if (coords.Row < 0 || coords.Column < 0 || coords.Row > (Size-1) || coords.Column > (Size-1)) throw new IndexOutOfRangeException();
+            if (coords.Row < 0 || coords.Column < 0 || coords.Row > (Size-1) || coords.Column > (Size-1))
+                throw new IndexOutOfRangeException("Specified square does not exist!");
 
             return this.Grid[coords.Row, coords.Column].Piece;
         }
 
         public void PutPiece(int row, int column, BoardPiece piece)
         {
-            if (row < 0 || column < 0 || row > (Size-1) || column > (Size-1)) throw new IndexOutOfRangeException();
+            if (row < 0 || column < 0 || row > (Size-1) || column > (Size-1))
+                throw new IndexOutOfRangeException("Specified square does not exist!");
+            if (piece.Equals(Factory.GetEmptyPiece()))
+                throw new ArgumentException("Cannot put an empty piece!");
 
             this.Grid[row, column].Piece = piece;
+            this.PiecesInGame.Add(new Coordinates(row, column), piece);
         }
 
         public void PutPiece(Coordinates coords, BoardPiece piece)
         {
-            if (coords.Row < 0 || coords.Column < 0 || coords.Row > (Size-1) || coords.Column > (Size-1)) throw new IndexOutOfRangeException();
+            if (coords.Row < 0 || coords.Column < 0 || coords.Row > (Size-1) || coords.Column > (Size-1))
+                throw new IndexOutOfRangeException("Specified square does not exist!");
+            if (piece.Equals(Factory.GetEmptyPiece()))
+                throw new ArgumentException("Cannot put an empty piece!");
 
             this.Grid[coords.Row, coords.Column].Piece = piece;
+            this.PiecesInGame.Add(coords, piece);
         }
 
-        public void MakeMove()
+        public void RemovePiece(int row, int column)
         {
-            
+            this.Grid[row, column].Piece = Factory.GetEmptyPiece();
+            this.PiecesInGame.Remove(new Coordinates(row, column));
+        }
+
+        public void RemovePiece(Coordinates coords)
+        {
+            this.Grid[coords.Row, coords.Column].Piece = Factory.GetEmptyPiece();
+            this.PiecesInGame.Remove(coords);
         }
 
         // Should belong to View but it's a temporary solution
